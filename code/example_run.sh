3a57d45de7dcf2ba6d5bbfc119ffa906e6f1d8a7
mattjdavis <matt.davis@alleninstitute.org>
2026-03-03 20:29:18 +0000

    run example

--- code/example_run.sh
+++ code/example_run.sh
@@ -3,11 +3,12 @@
 
 # This script demonstrates how to run the refactored taxonomy mapper
 # with the same parameters as the original map_taxonomy.py script
-
+    #--output-name 767018_Oregano_251104_inhibitory_clustered_cellxgene_lognorm_runners_up \
+    #--input-csv /root/capsule/data/HCR_767018_Oregano_251104/767018_Oregano_251104_inhibitory_clustered_cellxgene_lognorm.csv \
 python run_taxonomy_mapper.py \
     --config /root/capsule/code/params.json \
-    --input-csv /root/capsule/data/HCR_767018_Oregano_251104/767018_Oregano_251104_inhibitory_clustered_cellxgene_lognorm.csv \
-    --output-name 767018_Oregano_251104_inhibitory_clustered_cellxgene_lognorm_runners_up \
+    --input-csv /root/capsule/scratch/pairwise_unmix_input/oregano_inh_log2.csv \
+    --output-name 767018_Oregano_pairwise_unmix_inhibitory \
     --log-norm-data \
     --drop-layers VISp6a VISp6b \
     --bootstrap-iteration 100 \
